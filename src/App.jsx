464c3a6879dfc409ac4f7c10967ecf8769c5b892
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-07 08:44:13 +0600

    Setup React Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,28 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      { part: "/cart", element: <Cart /> },
+      { part: "/order/new", element: <CreateOrder /> },
+      { part: "/order/:orderId", element: <Order /> },
+    ],
   },
 ]);
 
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <h1>content</h1>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
