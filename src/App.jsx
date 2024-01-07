7f80a2b8835679e284b26a222432b4bb312fc3af
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-07 08:23:22 +0600

    makeing file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
-function App() {
-  return <h2> Hello world </h2>;
-}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-export default App;
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
+
+export default function App() {
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Layout />}>
+    //       <Route index element={<Home />} />
+    //       <Route path="blogs" element={<Blogs />} />
+    //       <Route path="contact" element={<Contact />} />
+    //       <Route path="*" element={<NoPage />} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+
+    <RouterProvider router={router} />
+  );
+}
