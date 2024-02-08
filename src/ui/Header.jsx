e78ad5cfa64e94892dbf65495a8a6d33bd0d9485
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-09 01:44:15 +0600

    Building the App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/"> Fast React Pizza Co. </Link>
+      <p> Sipmle Pizza P </p>
+    </header>
+  );
+};
+export default Header;
