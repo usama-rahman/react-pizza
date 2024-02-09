235e08ea4fa616231e2f2bc6730fc3f3f8b75223
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-10 04:33:59 +0600

    Reading and Updating the User State

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -25,7 +25,7 @@ async function fetchAddress() {
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  username: 'Usama',
+  username: '',
 };
 
 const userSlice = createSlice({
