041dcccd1b1d96684eefd5c7e61f842d94a328e2
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-17 04:41:54 +0600

    Updating Cart Quantities

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -4,6 +4,7 @@ import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
 import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
@@ -46,7 +47,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isinCart && <DeleteItem pizzaId={id} />}
+          {isinCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
           {!soldOut && !isinCart && (
             <Button type="small" onClick={handleAddToClick}>
               Add to cart
